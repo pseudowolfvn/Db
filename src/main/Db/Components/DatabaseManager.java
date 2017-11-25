c5ba856b85343df5bb593260f46644ac5743b04c
Dmytro Katrychuk <pseudowolf.vn@gmail.com>
2017-11-26 01:46:41 +0200

    add session caching to DatabaseManager

--- src/main/Db/Components/DatabaseManager.java
+++ src/main/Db/Components/DatabaseManager.java
@@ -4,14 +4,32 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class DatabaseManager {
     static String path = "./out/db";
 
+    static HashMap<String, Database> session = new HashMap<>();
+
+    public static void endSession() {
+        for (Database db: session.values()) {
+            try {
+                db.save();
+            } catch (IOException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+
+    public static void setPath(String path) {
+        DatabaseManager.path = path;
+    }
+
     public static Database createDatabase(String name) throws IOException {
         Database db = new Database(path, name);
-        db.save();
+        session.put(name, db);
         return db;
     }
 
@@ -24,7 +42,7 @@ public class DatabaseManager {
                 String[] filenameTokens = file.getName().split("\\.(?=[^\\.]+$)");
                 System.out.println(filenameTokens[0]);
                 if (filenameTokens.length > 1
-                        && filenameTokens[1].equalsIgnoreCase("db"))
+                        && filenameTokens[1].equalsIgnoreCase("db") || true)
                     names.add(filenameTokens[0]);
             }
         }
@@ -32,9 +50,12 @@ public class DatabaseManager {
         return names;
     }
     public static Database getDatabase(String name) {
+        if (session.containsKey(name))
+            return session.get(name);
         Database db = null;
         try{
             db = Database.load(path, name);
+            session.put(name, db);
         }
         catch (Exception ex) {
             System.out.println(ex.getMessage());
@@ -71,6 +92,9 @@ public class DatabaseManager {
                         file.delete();
             }
         }
+
+        if (session.containsKey(name))
+            session.remove(name);
     }
 
     public static void renameDatabase(String oldName, String newName) throws IOException {
@@ -85,6 +109,12 @@ public class DatabaseManager {
                         && filenameTokens[0].equalsIgnoreCase(oldName))
                     Files.move(file.toPath(), file.toPath().resolveSibling(newName + ".db"));
             }
+        }
+
+        if (session.containsKey(oldName)) {
+            Database db = session.get(oldName);
+            session.remove(oldName);
+            session.put(newName, db);
         }
     }
 }
